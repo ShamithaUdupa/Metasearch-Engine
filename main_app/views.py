c2dcf81e775f4092caa6c6f22eb598384f545e41
SanjanaBK <sanju.bhaskar@ymail.com>
2017-10-25 15:38:38 +0530

    Updated views.py Added bing search with ,merging

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from multiprocessing import Pool
 import urllib2
+import urllib
 import urlparse
 from bs4 import BeautifulSoup
 import numpy as np
 import math
 import random 
+from collections import OrderedDict
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
@@ -12,7 +15,7 @@ def population(num):
 	chromosomes=[]
 	for j in range(0,num):
 		chromosomes.append([]);
-		for s in range(0,10):
+		for s in range(0,15):
 			a=random.random()
 			if a<0.3:
 				chromosomes[j].append(0)
@@ -73,19 +76,58 @@ def fitness(chromosomes):
 	fit=np.array(fit)
 	return fit
 
+def merge_dict(dict_g,dict_b):
+	merged = []
+	for u in dict_b.items():
+		if u[0] not in dict_g:
+			dict_g[u[0]] = u[1] 
+	
+	dict_g = sorted(dict_g.iteritems(), key=lambda (k,v): (v,k))
+	for i in  dict_g:
+		merged.append(i[0])	
+	return merged
+			
 def result(request):
 	if request.method == 'GET':
 		string=request.GET.get('searchbox')
-		string="http://google.com/search?q="+string
+		string_bing = "http://www.bing.com/search?q=%s" % (urllib.quote_plus(string))
+		print string_bing
+		string="http://google.com/search?q=%s" % (urllib.quote_plus(string))
 		url=opener.open(string)
+		print string
 		content=url.read()
-		soup=BeautifulSoup(content,"lxml")
+		#print content
+		soup=BeautifulSoup(content,"lxml")	
 		title=soup.find_all("h3",{"class":"r"})
-		obj=[]
+		dict_g={}
+		print title
+		rank = 0		
 		for t in title:
 			x=t.find('a').get('href')
 			data=urlparse.parse_qs(urlparse.urlparse(x).query)
-			obj.append(scrape(t.find('a').getText(),data['q'][0]))
+			print x,'   x  \n',data,'  -----data----\n '	
+			print data			
+			dict_g[scrape(t.find('a').getText(),data['q'][0])] = rank
+			rank = rank + 1
+		
+		
+		url = opener.open(string_bing)
+		content = url.read()
+		#print content
+		soup=BeautifulSoup(content,"lxml")
+		#print soup.prettify()	
+		title=soup.find_all("li",{"class":"b_algo"})
+		dict_b = {}
+		rank = 0
+		for t in title:
+			print t			
+			x=t.find('a').get('href')	
+			data = t.find('a').getText()			
+			dict_b[scrape(data,x)] = rank
+			rank = rank + 1
+
+		obj = merge_dict(dict_g,dict_b)				
+		print obj		
 		chromosomes=population(2)
 		#print(chromosomes)
 		c=np.array(chromosomes)
@@ -96,12 +138,16 @@ def result(request):
 		c=np.array(chromosomes)
 		fit=fitness(c)
 		obj=np.array(obj)
+		print obj.shape,' ', obj
 		if(fit[0]>fit[1]):
-			obj=obj[c[0]==1]
+			obj=obj[np.where(c[0]==1)]
 			print(c[0])
 		else:
-			obj=obj[c[1]==1]
+			obj=obj[np.where(c[1]==1)]
 			print(c[1])
+		
 		return render(request,'result.html',{'context':obj})
 	else:
 		return render(request,'index.html')
+
+		
