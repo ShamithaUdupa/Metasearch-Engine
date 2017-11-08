75afcf60882322f885aaba7857f24f0318e030d1
Shamitha Udupa <shamithasudupa@gmail.com>
2017-11-08 08:42:38 +0530

    Update urls.py

--- metasearch/urls.py
+++ metasearch/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from main_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$',auth_views.login, name='login'),
+    url(r'^logout/$',auth_views.logout,name='logout'),
     url(r'^$',views.index),
     url(r'^result/',views.result),
 ]
