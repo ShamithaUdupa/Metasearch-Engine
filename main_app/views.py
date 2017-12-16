b8a51ddaa7587c86799190fe2ba6311f06fd012b
Shamitha Udupa <shamithasudupa@gmail.com>
2017-12-16 09:28:46 +0530

    Update views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.shortcuts import render_to_response
-=======
-=======
 from django.shortcuts import render_to_response
->>>>>>> 8c3249f214d3a44ab5bfc9381115030dbd9222e0
 from multiprocessing import Pool
->>>>>>> c2dcf81e775f4092caa6c6f22eb598384f545e41
 import urllib2
 import urllib
 import urlparse
@@ -15,19 +9,11 @@ from bs4 import BeautifulSoup
 import numpy as np
 import math
 import random 
-<<<<<<< HEAD
-<<<<<<< HEAD
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from models import Store
-=======
-from collections import OrderedDict
->>>>>>> c2dcf81e775f4092caa6c6f22eb598384f545e41
-=======
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from models import Store
 from collections import OrderedDict
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
->>>>>>> 8c3249f214d3a44ab5bfc9381115030dbd9222e0
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 obj=[]
@@ -43,21 +29,6 @@ def population(num,doc):
 				chromosomes[j].append(1)
 	return chromosomes
 
-<<<<<<< HEAD
-=======
-def population(num):
-	chromosomes=[]
-	for j in range(0,num):
-		chromosomes.append([]);
-		for s in range(0,15):
-			a=random.random()
-			if a<0.3:
-				chromosomes[j].append(0)
-			else:
-				chromosomes[j].append(1)
-	return chromosomes
-
->>>>>>> c2dcf81e775f4092caa6c6f22eb598384f545e41
 def crossover(c,num):
 	selected=np.random.randint(2,size=(num,))
 	orig=c
@@ -171,30 +142,6 @@ class scrape:
 		self.title=title
 		self.link=link
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def fitness1(chromosomes):
-=======
-=======
-def fitness1(chromosomes):
-
->>>>>>> 8c3249f214d3a44ab5bfc9381115030dbd9222e0
-def fitness(chromosomes):
->>>>>>> c2dcf81e775f4092caa6c6f22eb598384f545e41
-	fit=[]
-	for j in range(0,chromosomes.shape[0]):
-		val=0
-		for k in range(0,chromosomes.shape[1]):
-			if(chromosomes[j][k]==1):
-				val+=((j+1)*(chromosomes.shape[1]-k))
-		fit.append(val)
-	fit=np.array(fit)
-	return fit
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8c3249f214d3a44ab5bfc9381115030dbd9222e0
 def result(request):
 	if request.method == 'GET':
 		searchstring=request.GET.get('searchbox')
@@ -267,88 +214,3 @@ def result(request):
 			return render(request,'result.html',{'context':context})
 	else:
 		return render(request,'index.html')
-
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> 8c3249f214d3a44ab5bfc9381115030dbd9222e0
-def merge_dict(dict_g,dict_b):
-	merged = []
-	for u in dict_b.items():
-		if u[0] not in dict_g:
-			dict_g[u[0]] = u[1] 
-	
-	dict_g = sorted(dict_g.iteritems(), key=lambda (k,v): (v,k))
-	for i in  dict_g:
-		merged.append(i[0])	
-	return merged
-			
-def result(request):
-	if request.method == 'GET':
-		string=request.GET.get('searchbox')
-		string_bing = "http://www.bing.com/search?q=%s" % (urllib.quote_plus(string))
-		print string_bing
-		string="http://google.com/search?q=%s" % (urllib.quote_plus(string))
-		url=opener.open(string)
-		print string
-		content=url.read()
-		#print content
-		soup=BeautifulSoup(content,"lxml")	
-		title=soup.find_all("h3",{"class":"r"})
-		dict_g={}
-		print title
-		rank = 0		
-		for t in title:
-			x=t.find('a').get('href')
-			data=urlparse.parse_qs(urlparse.urlparse(x).query)
-			print x,'   x  \n',data,'  -----data----\n '	
-			print data			
-			dict_g[scrape(t.find('a').getText(),data['q'][0])] = rank
-			rank = rank + 1
-		
-		
-		url = opener.open(string_bing)
-		content = url.read()
-		#print content
-		soup=BeautifulSoup(content,"lxml")
-		#print soup.prettify()	
-		title=soup.find_all("li",{"class":"b_algo"})
-		dict_b = {}
-		rank = 0
-		for t in title:
-			print t			
-			x=t.find('a').get('href')	
-			data = t.find('a').getText()			
-			dict_b[scrape(data,x)] = rank
-			rank = rank + 1
-
-		obj = merge_dict(dict_g,dict_b)				
-		print obj		
-		chromosomes=population(2)
-		#print(chromosomes)
-		c=np.array(chromosomes)
-		fit=fitness(c)
-		#chromosomes=selection(chromosomes,fit)
-		chromosomes=crossover(chromosomes,2)
-		chromosomes=mutation(chromosomes,2)
-		c=np.array(chromosomes)
-		fit=fitness(c)
-		obj=np.array(obj)
-		print obj.shape,' ', obj
-		if(fit[0]>fit[1]):
-			obj=obj[np.where(c[0]==1)]
-			print(c[0])
-		else:
-			obj=obj[np.where(c[1]==1)]
-			print(c[1])
-		
-		return render(request,'result.html',{'context':obj})
-	else:
-		return render(request,'index.html')
-<<<<<<< HEAD
-
-		
->>>>>>> c2dcf81e775f4092caa6c6f22eb598384f545e41
-=======
->>>>>>> 8c3249f214d3a44ab5bfc9381115030dbd9222e0
